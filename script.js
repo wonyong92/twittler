13d3eda35a8b0fa380514ea25e3284ccba4c0ec3
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-04-29 12:25:58 +0900

    날짜 함수 문제 발생 롤백, 날짜 위치 변경

--- script.js
+++ script.js
@@ -21,9 +21,8 @@ function getRandomInt(min, max) {
 
 function getYmd10() {
   //yyyy-mm-dd 포맷 날짜 생성
-  var d = new Date(new Date() + 3240 * 10000).toISOString().replace("T", " ").replace(/..*/, ''); 
-  return d;
-  //return d.getFullYear() + "-" + ((d.getMonth() + 1) > 9 ? (d.getMonth() + 1).toString() : "0" + (d.getMonth() + 1)) + "-" + (d.getDate() > 9 ? d.getDate().toString() : "0" + d.getDate().toString()) +" "+ d.getHours() +":"+d.getMinutes()+":"+d.getSeconds();
+  var d = new Date();
+  return d.getFullYear() + "-" + ((d.getMonth() + 1) > 9 ? (d.getMonth() + 1).toString() : "0" + (d.getMonth() + 1)) + "-" + (d.getDate() > 9 ? d.getDate().toString() : "0" + d.getDate().toString()) +" "+ d.getHours() +":"+d.getMinutes()+":"+d.getSeconds();
 }
 
 function generateNewTweet() {
